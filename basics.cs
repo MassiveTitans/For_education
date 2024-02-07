befcf1d46a0c475698bdab8a0b37f938dcfa0838
Yaxshiliqov Javlon <97624379+MassiveTitans@users.noreply.github.com>
2024-02-07 11:17:22 +0500

    Add files via upload

---
+++ basics.cs
@@ -0,0 +1,81 @@
+// 1- topshiriq
+
+// namespace Name
+// {
+//     class Program
+//     {
+//         public static void Main() {
+
+//             int i = 1;
+//             int sum = 0;
+//             Boolean FLAG = true;
+
+//             while (FLAG) {
+//                 if (i < 1000)
+//                 {
+//                     Console.WriteLine(i);
+//                     sum = sum + i;
+//                     i = i + 2 ;
+                    
+
+//                 }
+//                 else{
+//                     Console.WriteLine(sum);
+//                     FLAG = false;
+//                 }
+//             }
+//         } 
+//     }
+// }
+
+
+
+
+
+
+// 2- topshiriq
+
+// namespace space_ {
+
+//     class Pro {
+
+
+//         static void Main(){
+
+//             int[] array = { 5, 3, 8, 2, 7, 1, 9, 4, 6 };
+
+//             int max_value = array.Max();
+//             int min_value =  array.Min();
+
+//             Console.WriteLine(max_value*min_value);
+//         }
+//     }
+// }
+
+
+
+
+
+// 3 - topshiriq
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         Console.Write("Butun son kiriting: ");
+//         int x = Convert.ToInt32(Console.ReadLine());
+
+//         long factorial = 1;
+
+//         // Factorialni hisoblash
+//         for (int i = 1; i <= x; i++)
+//         {
+//             factorial *= i;
+//         }
+
+//         // Natijani ekranga chiqarish
+//         Console.WriteLine($"{x}! = {factorial}");
+//     }
+// }
+
+
